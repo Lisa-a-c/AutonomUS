0f944f0d5f643d10ec67236988aface7deaa7283
Elisa de Almeida <elisa.a.cardoso2024@gmail.com>
2025-04-26 11:03:51 -0300

    mobile

---
+++ mobile-p/app/index.tsx
@@ -0,0 +1,56 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Image } from 'expo-image';
+
+const logo = require('@/assets/images/logoAUTONOMUS.png');
+const nome = require('@/assets/images/nomeLogo.png');
+
+// estrutura da tela
+export default function Index() {
+  return (
+    <View style={styles.container}> 
+     <View style={styles.imageContainer}>
+        <Image source={logo} style={styles.logo} />
+      </View>
+      <View style={styles.imageContainer}> 
+      <Image source={nome} style={styles.nome} />
+      </View>
+        <Text style={styles.text}>Sua ponte entre demanda e solução</Text>
+    </View>
+  );
+}
+
+//Desing 
+const styles =  StyleSheet.create( {
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+ 
+  text: {
+    color: '#727272',
+    fontSize: 15,
+  },
+  
+  button : {
+    fontSize: 20,
+    color:'#FFC100',
+    textDecorationLine: 'underline',
+    
+  },
+  //imagens 
+  logo: {
+    width: 159,
+    height: 131,
+  },
+  nome: {
+    width:269,
+    height:52,
+  },
+  imageContainer: {
+    flex: 1,
+  },
+
+});
