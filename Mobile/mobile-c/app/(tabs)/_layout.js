0b2477b71e4c6943091631f51d75c24eea564f7c
JuLCM <juliana.castro37@etec.sp.gov.br>
2025-05-14 12:19:02 -0300

    esqueci senha

--- Mobile/mobile-c/app/(tabs)/_layout.js
+++ Mobile/mobile-c/app/(tabs)/_layout.js
@@ -8,6 +8,7 @@ export default function RootLayout() {
         <Stack.Screen name="index" options={{ title: 'Home' }} />
         <Stack.Screen name="login" options={{ title: 'Login' }} />
         <Stack.Screen name="intoPrestador" options={{ title: 'Intro' }} />
+        <Stack.Screen name="esqueciSenha" options={{ title: 'Esqueci' }} />
       </Stack>
     );
 }
--- Mobile/mobile-c/app/(tabs)/esqueciSenha.js
+++ Mobile/mobile-c/app/(tabs)/esqueciSenha.js
@@ -4,17 +4,16 @@ import { Image, ImageBackground } from "expo-image";
 //import { CiMail } from "react-icons/ci";
 import styles from "@/styles/LoginStyles";
 //import Button from '@/componentes/Button';
-import { Link } from "expo-router";
+//import { Link } from 'expo-router';
 
 //IMAGENS
 const logo = require("@/assets/images/Logo.png");
 const nome = require("@/assets/images/Nome.png");
-const personagem = require("@/assets/images/CharacterLogin.png");
+const personagem = require("@/assets/images/CharacterEqueci.png");
 const blur = require("@/assets/images/Blur.png");
 
-export default function Login() {
+export default function EsqueciSenha() {
   const { email, onChangeEmail } = React.useState("Email");
-  const { senha, onChangeSenha } = React.useState("senha");
 
   return (
     <View style={styles.container}>
@@ -23,7 +22,9 @@ export default function Login() {
         <Image source={nome} style={styles.nome} />
       </View>
       <View style={styles.view2container}>
-        <Image source={personagem} style={styles.personagem} />
+        <ImageBackground source={blur} style={styles.blur}>
+          <Image source={personagem} style={styles.personagem} />
+        </ImageBackground>
       </View>
       <View style={styles.view3container}>
         <TextInput
@@ -32,14 +33,7 @@ export default function Login() {
           value={email}
           placeholder="Email"
         />
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeSenha}
-          value={senha}
-          placeholder="Senha"
-        />
-
-        <Link href="/esqueciSenha" asChild>
+        <Link href="/login" asChild>
           <View style={styles.forgotContainer}>
             <Pressable style={styles.forgotBTN}>
               <Text style={styles.forgot}>ESQUECI MINHA SENHA</Text>
@@ -47,12 +41,10 @@ export default function Login() {
           </View>
         </Link>
 
-        <View className="Button" style={styles.btnContainer}>
-          <View style={styles.buttonContainer}>
-            <Pressable style={styles.button}>
-              <Text style={styles.text}>ENTRAR</Text>
-            </Pressable>
-          </View>
+        <View style={styles.buttonContainer}>
+          <Pressable style={styles.button}>
+            <Text style={styles.text}>ENTRAR</Text>
+          </Pressable>
         </View>
       </View>
 
